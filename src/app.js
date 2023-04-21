a6f30dbd1185d7f7b977e324bc9b83e5f05849b3
Cserhati <cserhatiDavid4444@gmail.com>
2023-04-21 09:07:25 +0200

    tarolas

--- src/app.js
+++ src/app.js
@@ -8,3 +8,56 @@
 * Licenc: GNU GPL
 * Nem vállalunk felelősséget a rossz jegyekért
 */
+const doc={
+
+}
+window.addEventListener('load',()=>{
+    init();
+})
+function init() {
+    doc.setButton.addEventListener('click',()=>{
+        startSet();
+    })
+    doc.setButton.addEventListener('click',()=>{
+        startGet();
+    })
+}
+
+//local storage
+function startSet() {
+    let emp={
+        name:"Béla",
+        city:"Szeged",
+        salary:256
+    }
+    let empStr=JSON.stringify(emp)
+    localStorage.setItem("employee",empStr)
+}
+function startGet() {
+    let empStr=localStorage.getItem("employee")
+    let emp=JSON.parse(empStr)
+    console.log(emp.name)
+}
+
+/*
+//session storage
+function startSet() {
+    let pass="titok"
+    sessionStorage.setItem("jelszó",pass)
+}
+function startGet() {
+    let pass=sessionStorage.getItem("jelszó")
+    console.log(pass)
+}
+*/
+/*
+//cookies
+function startSet() {
+    let pass="titok"
+    document.cookie="jelszo="+pass+"; max-age="+60;
+}
+function startGet() {
+    let pass=document.cookie
+    console.log(pass)
+}
+*/
